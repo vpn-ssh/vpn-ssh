2899537aa06ea4958fc5c2c3e18fe81e41a9b4a6
Couty <100492902+vpn-ssh@users.noreply.github.com>
2022-05-29 17:53:22 -0300

    Update ssh5.sh

--- ssh5.sh
+++ ssh5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/CoutySSH/SSH/main/mult11; chmod 777 mult11;./mult11
+wget https://raw.githubusercontent.com/CoutySSH/SSH/main/mult11 /usr/bin/; chmod 777 /usr/bin/mult11;./mult11
